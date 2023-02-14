853ca93fb0cf6a4bc8bc4c2a66c891ce7f12c7c6
Hasnain-20 <123731247+Hasnain-20@users.noreply.github.com>
2023-02-14 12:00:17 +0500

    Add files via upload

--- News/Politico.py
+++ News/Politico.py
@@ -38,10 +38,14 @@ class PoliticoEU:
         self.authorImg = []
         self.keyUsed = []
         self.authUrls = []
+        self.article_Url = []
+        self.authorsBio = []
 
     def searchKeywords(self):
         for key in self.keywords:
-            for i in range(0,1):
+            print(key)
+            for i in range(0,50):
+                print(i,end=", ", flush=True)
                 response = requests.post(
                     'https://a3cxkoqgf3-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(4.14.2)%3B%20Browser%20(lite)%3B%20instantsearch.js%20(4.47.0)%3B%20JS%20Helper%20(3.11.1)&x-algolia-api-key=138da0d7b154a5032735cfdcfda0e3cf&x-algolia-application-id=A3CXKOQGF3',
                     headers=self.headers,
@@ -55,6 +59,7 @@ class PoliticoEU:
                         self.keyUsed.append(key.replace("%20"," "))
                         self.newsPlatform.append(self.platform)
                     self.responses.append(response)
+            print()
 
     def parseSearchResults(self):
         for resp in self.responses:
@@ -63,13 +68,24 @@ class PoliticoEU:
                 auths = j['authors']
                 temp = []
                 temp2 = []
+                temp3 = []
                 for aut in auths:
                     temp2.append(aut['url'].replace('author','staff'))
+                    temp3.append(aut['url'])
                     temp.append(aut['name'])
+                self.article_Url.append(j['url'])
                 self.article_Title.append(j['title']['en'])
-                self.authors.append(temp)
                 self.urls.append(temp2)
-                self.authUrls.append(j['authors'])
+                if temp3:
+                    if len(temp3)>1:
+                        self.authors.append(temp)
+                        self.authUrls.append(temp3)
+                    else:
+                        self.authUrls.append(temp3[0])
+                        self.authors.append(temp[0])
+                else:
+                    self.authors.append("")
+                    self.authUrls.append("")
                 # print(article_Title[-1], authors[-1], urls[-1])
         print(len(self.urls))
 
@@ -81,9 +97,10 @@ class PoliticoEU:
             lis1 = []
             lis2 = []
             lis3 = []
+            lis4 = []
             for resp in respons:
+                print(kkk, " : ", resp.status_code, resp.url)
                 if resp.status_code == 200:
-                    print(kkk, " : ", resp.url)
                     soup = BeautifulSoup(resp.text, 'html.parser')
                     try:
                         lis1.append(soup.select_one('.speaker__twitter-url')['href'])
@@ -97,10 +114,28 @@ class PoliticoEU:
                         lis3.append(soup.select_one('.speaker__avatar>img')['src'])
                     except:
                         lis3.append("")
-            self.twitter.append(lis1)
-            self.email.append(lis2)
-            self.authorImg.append(lis3)
+                    try:
+                        lis4.append(soup.select_one('.speaker__intro').text.replace('\n',"").replace('\x0a',' '))
+                    except:
+                        lis4.append("")
+                else:
+                    lis1.append("")
+                    lis2.append("")
+                    lis3.append("")
+                    lis4.append("")
+            self.twitter.append(self.checker(lis1))
+            self.email.append(self.checker(lis2))
+            self.authorImg.append(self.checker(lis3))
+            self.authorsBio.append(self.checker(lis4))
             kkk=kkk+1
+
+    def checker(self, lis):
+        if len(lis)==1:
+            return lis[0]
+        if not any(lis):
+            return ""
+        else:
+            return lis
 
     def decodeEmail(self,fp):
         try:
@@ -117,16 +152,20 @@ class PoliticoEU:
         return self.makeCSV()
     
     def makeCSV(self):
-        # print(len(self.article_Title))
-        # print(len(self.authors))
-        # print(len(xself.twitter))
-        # print(len(self.email))
-        # print(len(self.authorImg))
-        # print(len(self.keyUsed))
-        # print(len(self.newsPlatform))
-        df = pd.DataFrame({"Platform" : self.newsPlatform,"Article Title" : self.article_Title, "Keyword" : self.keyUsed, 
-                           "Authors Name": self.authors,"Authros Email" : self.email, 
-                           "Authors Twitter" : self.twitter, "Authors Image" : self.authorImg, "Authors Page Url" : self.authUrls})
+        print(len(self.article_Title))
+        print(len(self.authors))
+        print(len(self.twitter))
+        print(len(self.email))
+        print(len(self.authorImg))
+        print(len(self.keyUsed))
+        print(len(self.newsPlatform))
+        print(len(self.article_Url))
+        print(len(self.authUrls))
+        print(len(self.authorsBio))
+        df = pd.DataFrame({"Platform" : self.newsPlatform, "Article Title" : self.article_Title, "Article Url" : self.article_Url,
+                            "Keyword" : self.keyUsed, "Authors Name": self.authors,"Authros Email" : self.email, 
+                           "Authors Twitter" : self.twitter, "Authors Image" : self.authorImg, "Authors Bio" : self.authorsBio,
+                           "Authors Page Url" : self.authUrls})
         df.to_csv(f'{self.platform}.csv', encoding='utf-8-sig', index=False)
         return df
 
